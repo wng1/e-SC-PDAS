4ece5afd20398ef42d32ddc1ce39d6621b25f481
wng1 <w.ng1@ncl.ac.uk>
2015-07-30 15:51:21 +0100

    Update to OphidiaContainer main.sh
    One of the main.sh for one block

--- code/e-SC-blocks/ShellWrapperTest/src/main/scripts/main.sh
+++ code/e-SC-blocks/ShellWrapperTest/src/main/scripts/main.sh
@@ -2,4 +2,9 @@
 
 /usr/bin/printenv
 
+OUTFILE=$( mktemp output-XXXX.csv )
+echo $OUTFILE >> $OUTPUT__output_files
+
+$DEP__OphidiaTerminal__oph_term oph_term -u $1 $PROP__u -p $2 $PROP__p -H $3 $PROP__H -P $4 $PROP__P -e "oph_createcontainer container=$PROP__container;dim=$PROP__dim;dim_type=$PROP__dim_type;hierarchy=$PROP__hierarchy;" | tee -a "$OUTFILE"
+##More parameters are required to be entered and further improvements to be made so the request commands do not accept nulls.
 ls
